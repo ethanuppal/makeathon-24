9444587df37d5f457dc0fa843a1978afe75232b8
Sidharth Rao <sidharthmrao@gmail.com>
2024-03-09 18:12:17 -0500

    Raspberry pi side serial connection.

--- servo/test.py
+++ servo/test.py
@@ -1,11 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 
+def start_power(power_pin):
+    # Set the pin as an output
+    GPIO.setup(power_pin, GPIO.OUT)
+
+    # Set the pin to high (3.3V)
+    GPIO.output(power_pin, GPIO.HIGH)
+
+# Function to set angle of the servo
+def set_angle(angle):
+    duty_cycle = (angle / 180.0) * (duty_cycle_max - duty_cycle_min) + duty_cycle_min
+    pwm.ChangeDutyCycle(duty_cycle)
+
+# ------------------------- #
+
+# Set pin numbers
+power_pin = 40
+control_pin = 37
+
 # Set GPIO mode
 GPIO.setmode(GPIO.BOARD)
 
-# Set pin numbers
-control_pin = 40
+# Start Power
+start_power(power_pin)
 
 # Set PWM parameters
 frequency = 50  # Hz (typical for servos)
@@ -16,12 +34,6 @@ duty_cycle_max = 12.5  # Duty cycle for 180 degrees
 GPIO.setup(control_pin, GPIO.OUT)
 pwm = GPIO.PWM(control_pin, frequency)
 pwm.start(0)
-
-# Function to set angle of the servo
-def set_angle(angle):
-    duty_cycle = (angle / 180.0) * (duty_cycle_max - duty_cycle_min) + duty_cycle_min
-    pwm.ChangeDutyCycle(duty_cycle)
-    time.sleep(0.3)  # Allow time for servo to reach position
 
 try:
     while True:
@@ -40,3 +52,10 @@ try:
 except KeyboardInterrupt:
     pwm.stop()
     GPIO.cleanup()
+
+# pwm.ChangeDutyCycle(10) # Tells the servo to turn to the left ( -90 deg position )
+# time.sleep(2) # Tells the servo to Delay for 5sec
+
+# pwm.stop()
+# GPIO.cleanup()
+
