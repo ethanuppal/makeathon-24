552a535bb7cca5de51f02f766dd8aa7f593bd9ac
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-03-10 00:43:00 -0500

    move telemetry

--- app/main.py
+++
@@ -1,9 +0,0 @@
-# Makeathon 2024 project
-# Utku, Sid, Andrew, Ethan
-
-from app import App
-
-if __name__ == "__main__":
-    app = App()
-    app.setup()
-    app.run()
--- app/servo/arduino/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Sidharth Rao, 2024
-
-arduino-cli compile --fqbn arduino:avr:uno *.ino
-arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno *.ino -v
