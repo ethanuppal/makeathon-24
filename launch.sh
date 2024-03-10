8534e811691aafd40a37f9edc78d20f7f053e906
Sidharth Rao <sidharthmrao@gmail.com>
2024-03-09 20:30:52 -0500

    Update launch

--- launch.sh
+++ launch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DISPLAY=:0
 DATAFILE="./data/happiness.txt"
 APPMAIN="./app/main.py"
+PY=$(which python3 || which pypy3 || which python || which pypy) 
 
 rm -f "$DATAFILE.backup"
 if [ -f "$DATAFILE" ]; then
     mv "$DATAFILE" "$DATAFILE.backup"
 fi
-$(which python3 || which pypy3 || which python || which pypy) "$APPMAIN" > "$DATAFILE"
+"$PY" "$APPMAIN" > "$DATAFILE" & "$PY" servo/serial_connect.py
