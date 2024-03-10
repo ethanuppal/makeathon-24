a7e50c576980dd7445d35d02fac0ff88ca6c9cdd
Yey007 <yeeeeeeey007@gmail.com>
2024-03-10 11:25:41 -0400

    Dashboard

--- dashboard.js
+++ dashboard.js
@@ -1,13 +1,24 @@
 // Copyright (C) 2024 Ethan Uppal. All rights reserved.
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js'
+import { getFirestore, onSnapshot, doc } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js'
 
 class Dashboard {
-    constructor(dashboardRootId, sourceInputId, sourceButtonId, sourceSpanId) {
+    constructor(dashboardRootId) {
         this.dashboardRoot = document.getElementById(dashboardRootId);
-        this.sourceInput = document.getElementById(sourceInputId);
-        this.sourceButton = document.getElementById(sourceButtonId);
-        this.sourceSpan = document.getElementById(sourceSpanId);
-        this.setupHTML()
-        this.configure();
+        this.setupHTML();
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyDyiPg6Dv_bJtCF_gettCwfTtSNFbibu3s",
+            authDomain: "makeathon-24.firebaseapp.com",
+            projectId: "makeathon-24",
+            storageBucket: "makeathon-24.appspot.com",
+            messagingSenderId: "997756541374",
+            appId: "1:997756541374:web:0e850aa65a2919beaa81ef"
+        };
+
+        this.app = initializeApp(firebaseConfig);
+        this.db = getFirestore(this.app);
+
         this.run();
     }
 
@@ -19,52 +30,13 @@ class Dashboard {
         this.canvas.style.width = '100%';
         this.canvas.style.height = '100%';
         this.canvas.style.display = 'block';
-
-        this.sourceButton.addEventListener('click', () => {
-            if (this.sourceInput.value.length > 0) {
-                this.updateSource(this.sourceInput.value);
-            }
-        }, false);
-    }
-
-    configure() {
-        if ('DashboardConfig' in window) {
-            if (!('source' in DashboardConfig)) {
-                this.updateSource('example/static_v1.json');
-            } else {
-                this.updateSource(DashboardConfig.source);
-            }
-            if (!('rate' in DashboardConfig)) {
-                this.rate = 1;
-            }
-        }
     }
 
     run() {
-        async function fetchJSON(url) {
-            try {
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return await response.json();
-            } catch (error) {
-                return null;
-            }
-        }
-
-        const updateBound = this.update.bind(this);
-        const intervalId = setInterval(() => {
-            fetchJSON(this.source)
-                .then(data => {
-                    if (data) {
-                        updateBound(data);
-                        this.sourceSpan.style.color = 'black';
-                    } else {
-                        this.sourceSpan.style.color = 'red';
-                    }
-                })
-        }, this.rate * 1000 /* milliseconds -> seconds */);
+        onSnapshot(doc(this.db, "happiness", "current"), (doc) => {
+            console.log(doc.data())
+            this.update(doc.data())
+        });
     }
 
     update(data) {
@@ -72,6 +44,7 @@ class Dashboard {
             console.error('Unknown source format');
             return;
         }
+
         switch (data.version) {
             case 1: {
                 this.updateV1(data.contents);
@@ -93,7 +66,7 @@ class Dashboard {
 
         const levels = [1, 2, 3, 4, 5];
         const colors = ['#ff0000', '#ff8000', '#80ff00', '#40ff00', '#00ff00'];
-        const counts = levels.map(level => contents.happiness[`${level}`]);
+        const counts = levels.map(level => contents[`${level}`]);
         const levelTextRects = levels.map(level => ctx.measureText(`${level} (${counts[level - 1]})`));
 
         const textHeight = 10;
@@ -125,8 +98,5 @@ class Dashboard {
 window.addEventListener('load', function () {
     new Dashboard(
         'telemetry-dashboard',
-        'source-url-input',
-        'source-url-button',
-        'source-url-span'
     );
 }, false);
