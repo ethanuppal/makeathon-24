c13d8c160e3468d5c029302efbdc31ad2dc4d3d4
Sidharth Rao <sidharthmrao@gmail.com>
2024-03-09 20:11:06 -0500

    Testing

--- servo/serial_connect.py
+++ servo/serial_connect.py
@@ -1,10 +1,21 @@
 import serial
 import time
 
-serial1 = serial.Serial('/dev/ttyACM0', 9600)
+serial = serial.Serial('/dev/ttyACM0', 9600)
+
+def to_bytes(x: int):
+    return x.to_bytes((x.bit_length() + 7) // 8, byteorder='big')
 
 while True:
-    serial1.write(b'0')
-    time.sleep(1)
-    serial1.write(b'9')
-    time.sleep(1)
+    # serial1.write(to_bytes(100))  # Write a hundred degree setpoint
+    # print(to_bytes(100))
+    # time.sleep(3)
+    # serial1.write(to_bytes(0))  # 0 Degree setpoint
+    # print(to_bytes(0))
+    # time.sleep(3)
+
+    serial.write(b'100')
+    time.sleep(3)
+    serial.write(b'0')
+    time.sleep(3)
+    
