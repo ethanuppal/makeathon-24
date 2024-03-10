2c44ff9f68abb942e5b6539b64ce709c5d32166f
Sidharth Rao <sidharthmrao@gmail.com>
2024-03-10 04:50:10 -0400

    no more log

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@
 APPMAIN="./app/main.py"
 PY=$(which python3 || which pypy3 || which python || which pypy) 
 
-DISPLAY=:0 "$PY" "$APPMAIN" $(uname) & "$PY" app/telemetry/server.py >> log.txt
+DISPLAY=:0 "$PY" "$APPMAIN" $(uname) & "$PY" app/telemetry/server.py
